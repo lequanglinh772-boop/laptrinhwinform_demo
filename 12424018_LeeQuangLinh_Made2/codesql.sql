88398f0613bb403bc3429192f20e765be652d0e8
lequanglinh772 <lequanglinh772@gmail.com>
2025-12-29 16:06:19 +0700

    code Sql de 2

---
+++ 12424018_LeeQuangLinh_Made2/codesql.sql
@@ -0,0 +1,26 @@
+﻿create database QLSP;
+go
+use QLSP;
+CREATE TABLE Category (
+    CategoryID INT IDENTITY(1,1) PRIMARY KEY,
+    CategoryName NVARCHAR(100) NOT NULL
+);
+CREATE TABLE Product (
+    ProductID INT IDENTITY(1,1) PRIMARY KEY,
+    ProductName NVARCHAR(200) NOT NULL,
+    Price DECIMAL(18,2) NOT NULL,
+    Quantity INT NOT NULL,
+    CategoryID INT NOT NULL,
+    CONSTRAINT FK_Product_Category 
+        FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
+);
+INSERT INTO Category(CategoryName)
+VALUES (N'Chip CPU'), (N'Ram Ai Siêu Đắt '), (N'SSD 10TB');
+
+INSERT INTO Product(ProductName, Price, Quantity, CategoryID)
+VALUES
+(N'Chip Nasa ', 900000, 10, 1),
+(N'Ram 10TB ', 450000, 15, 2),
+(N'SSD kèm case', 5500000, 5, 3);
+
+
